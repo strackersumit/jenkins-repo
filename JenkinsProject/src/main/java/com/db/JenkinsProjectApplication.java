a9d4bb6a43e45397cd3959a56e5462fc99110f4b
sumit <sumit@lenovo>
2024-07-24 20:27:05 +0530

    Revert "uploade latest project"
    This reverts commit 3775c05d9cb30bbbc53c8970908855a3adc7241c.

--- JenkinsProject/src/main/java/com/db/JenkinsProjectApplication.java
+++ JenkinsProject/src/main/java/com/db/JenkinsProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JenkinsAppApplication {
+public class JenkinsProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JenkinsAppApplication.class, args);
+		SpringApplication.run(JenkinsProjectApplication.class, args);
 	}
 
 }
--- JenkinsProject/src/test/java/com/db/JenkinsProjectApplicationTests.java
+++ JenkinsProject/src/test/java/com/db/JenkinsProjectApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JenkinsAppApplicationTests {
+class JenkinsProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
